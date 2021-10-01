6da3fb5ab7d3ac012c5f46a5b7706026d3913886
Zach Dive <zjhd201@exeter.ac.uk>
2021-10-01 17:21:20 +0100

    searchbasic

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { NavLink } from "react-router-dom";
+import Search from "./SearchBar";
 
 function ListProducts() {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
+
+ const filterProducts = (products, query) => {
+  if(!query) {
+    return products;
+  }
+  return products.filter(food => {
+    const foodName = food.name.toLowerCase();
+    return foodName.includes(query);
+  })
+}
+
+const { search } = window.location;
+const query = new URLSearchParams(search).get('search-bar');
+const [ searchInput, searchProductFilter ] = useState(query || '');
+const filteredProducts = filterProducts(products, searchInput);
+
+  
+
+useEffect(() => {
     async function getAllProjects() {
       const response = await axios.get(
         `${process.env.REACT_APP_SERVER_HOSTNAME}/products`,
@@ -17,9 +36,11 @@ function ListProducts() {
   }, []);
 
   return (
+    <div className="Mainfeed">
+    <Search searchInput={searchInput}  searchProductFilter = {searchProductFilter} />
     <ul>
-      <p>working</p>
-      {products.map((product) => {
+     
+      {filteredProducts.map((product) => {
         return (
           <li key={product._id}>
             <NavLink to={`/products/${product._id}`}><img src={product.imageUrl} width = "50px"  height = "50px"/></NavLink>
@@ -29,6 +50,7 @@ function ListProducts() {
         );
       })}
     </ul>
+    </div>
   );
 }
 
