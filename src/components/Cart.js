a7f07e6dd6c769246817f218626980f2c265b3af
Nunofgg <goncalvesnuno00@gmail.com>
2021-10-04 11:34:27 +0100

    merge

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,44 +3,72 @@ import React, { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { LoggedUserConsumer } from "../context/loggedUser";
 
-
 function Cart() {
-    const loggedInUser = useContext(LoggedUserConsumer);
-    const [userCart, setUserCart] = useState({});
+  const loggedInUser = useContext(LoggedUserConsumer);
+  const [userCart, setUserCart] = useState({});
 
-    //2
-    useEffect(() => {
-        async function getUser (){
-            const user = await axios.get(`${process.env.REACT_APP_SERVER_HOSTNAME}/usercart`, { withCredentials: true})
-            setUserCart(user.data);
-        } 
-        getUser();
-        
-    }, []);
-    
+  //2
+  useEffect(() => {
+    async function getUser() {
+      const user = await axios.get(
+        `${process.env.REACT_APP_SERVER_HOSTNAME}/usercart`,
+        { withCredentials: true }
+      );
+      setUserCart(user.data);
+    }
+    getUser();
+  }, []);
 
-    //1
-    //3
-    return (
-        <>
-        <h2>Cart</h2>
-        <ul>
-            {userCart.cart?.products.map((item, index) => {
-                return (
-                    <li key={index}>
-                        <NavLink to={`/products/${item.item._id}`}>
-                            <div>
-                                <h4>{item.item.name}</h4>
-                                <h6>{item.item.price}</h6>
-                                <p>{item.item.expire_in}</p>
-                            </div>
-                        </NavLink>
-                    </li>
-                );
-            })}
-        </ul>
-        </>
-    );
+  const handleDeleteButton = async () => {
+    await axios.put()
+  }
+
+  //1
+  //3
+  return (
+    <>
+      <h2>Cart</h2>
+      <ul>
+        {userCart.cart?.products.map((item, index) => {
+          const handleQuantityButton = async (statement) => {
+            const quantity = item.quantity;
+            const product = {
+                itemId: item._id,
+                quantity: quantity,
+                purchasePrice: `${item.price * quantity}`,
+            };
+            if (statement === "decrease") {
+                await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart.cart._id}`, product, { withCredentials: true })
+            } else if (statement === "increase") {
+                await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart.cart._id}`, product, { withCredentials: true })
+            }
+          };
+
+          return (
+            <li key={index}>
+                <div>
+                  <NavLink to={`/products/${item.item._id}`}>
+                    <h4>{item.item.name}</h4>
+                  </NavLink>
+                  <h6>{item.item.price}</h6>
+                  <p>{item.item.expire_in}</p>
+                  <div>
+                    <button onClick={() => handleQuantityButton("decrease")}>
+                        -
+                    </button>
+                    <p>{item.quantity}</p>
+                    <button onClick={() => handleQuantityButton("increase")}>
+                        +
+                    </button>
+                  </div>
+                  <button onClick={handleDeleteButton}>Remove</button>
+                </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default Cart;
