bf1f31e9e7d4568d604942a1304b299be4d5e7a7
Nunofgg <goncalvesnuno00@gmail.com>
2021-10-04 16:05:05 +0100

    cart almost done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { LoggedUserConsumer } from "../context/loggedUser";
 
 function Cart() {
-  const loggedInUser = useContext(LoggedUserConsumer);
   const [userCart, setUserCart] = useState({});
 
   //2
@@ -23,26 +21,29 @@ function Cart() {
     await axios.put()
   }
 
+  const handleQuantityButton = async (statement, item) => {
+    const product = {
+        itemId: item.item._id,
+        quantity: item.quantity,
+        purchasePrice: `${item.item.price * item.quantity}`,
+    };
+    if (statement === "decrease") {
+        const response = await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart._id}/decrease`, product, { withCredentials: true });
+        setUserCart(response.data);
+    } else if (statement === "increase") {
+        const response = await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart._id}/increase`, product, { withCredentials: true });
+        setUserCart(response.data);
+    }
+  };
+
   //1
   //3
   return (
     <>
       <h2>Cart</h2>
       <ul>
-        {userCart.cart?.products.map((item, index) => {
-          const handleQuantityButton = async (statement) => {
-            const quantity = item.quantity;
-            const product = {
-                itemId: item._id,
-                quantity: quantity,
-                purchasePrice: `${item.price * quantity}`,
-            };
-            if (statement === "decrease") {
-                await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart.cart._id}`, product, { withCredentials: true })
-            } else if (statement === "increase") {
-                await axios.put(`${process.env.REACT_APP_SERVER_HOSTNAME}/cart/${userCart.cart._id}`, product, { withCredentials: true })
-            }
-          };
+        {userCart && userCart.products?.map((item, index) => {
+         
 
           return (
             <li key={index}>
@@ -53,11 +54,11 @@ function Cart() {
                   <h6>{item.item.price}</h6>
                   <p>{item.item.expire_in}</p>
                   <div>
-                    <button onClick={() => handleQuantityButton("decrease")}>
+                    <button onClick={() => handleQuantityButton("decrease", item)}>
                         -
                     </button>
                     <p>{item.quantity}</p>
-                    <button onClick={() => handleQuantityButton("increase")}>
+                    <button onClick={() => handleQuantityButton("increase", item)}>
                         +
                     </button>
                   </div>
@@ -65,6 +66,7 @@ function Cart() {
                 </div>
             </li>
           );
+
         })}
       </ul>
     </>
