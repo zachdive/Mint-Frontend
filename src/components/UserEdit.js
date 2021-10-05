465002aa943b4d78f8eea3be70dc4a6bd2f9f6d6
João Monteiro <jfcm.monteiro@gmail.com>
2021-10-05 16:12:50 +0100

    user edit

--- src/components/UserEdit.js
+++ src/components/UserEdit.js
@@ -15,30 +15,31 @@ function UserEdit({match}) {
     //
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
-    const [phoneNumber, setphoneNumber] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
     const [farmerAdress, setFarmerAdress] = useState("");
     const [city, setCity] = useState("");
     const [zipCode, setZipCode] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
 
     const history = useHistory();
   
     useEffect(() => {
-      async function getProject() {
-        const project = await axios.get(
-          `${process.env.REACT_APP_SERVER_HOSTNAME}/user/${match.params.id}`
-        );
+      async function getUser() {
+        const project = loggedInUser;
   
-        setFirstName(project.data.firstName);
-        setLastName(project.data.lastName); 
-        setphoneNumber(project.data.phoneNumber);
-        setFarmerAdress(project.data.farmerAdress);
-        setCity(project.data.city);
-        setZipCode(project.data.zipCode);    }
-      getProject();
+        setFirstName(project.firstName);
+        setLastName(project.lastName); 
+        setPhoneNumber(project.phoneNumber);
+        setFarmerAdress(project.farmerAdress);
+        setCity(project.city);
+        setZipCode(project.zipCode);
+        setImageUrl(project.imageUrl);    }
+      getUser();
     }, []);
   
     const handleFormSubmit = async (e) => {
       e.preventDefault();
+      debugger;
       const body = {
         firstName,
         lastName,
@@ -46,6 +47,7 @@ function UserEdit({match}) {
         farmerAdress,
         city,
         zipCode,
+        imageUrl,
       };
   
       await axios.put(
@@ -53,61 +55,50 @@ function UserEdit({match}) {
         body
       );
   
-      toast.success("Project updated");
-      history.push("/projects");
+      toast.success("User updated");
+      history.push("/user");
     };
-  
-    //
 
     return (
     <div className="row">
         <div className="col-md-3">
             <img src={loggedInUser.imageUrl} width="100px" height="100px" alt="user-img"/>
-            <Form.Group className="position-relative mb-3">
-              <Form.Label>Profile Picture</Form.Label>
-              <Form.Control
-                    type="file"
-                    required
-                    name="imageUrl"
-                    onChange={handleFormSubmit}
-              />
-            </Form.Group>
         </div>
         <div className="col-md-6">
-            <Form>
+            <Form onSubmit={handleFormSubmit}>
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridEmail">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="firstName" placeholder="Enter first name" value={loggedInUser.firstName}/>
+                    <Form.Control type="firstName" placeholder="Enter first name" value={firstName}  onChange={(e) => setFirstName(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridPassword">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="lastName" placeholder="Enter last name" value={loggedInUser.lastName}/>
+                    <Form.Control type="lastName" placeholder="Enter last name" value={lastName}  onChange={(e) => setLastName(e.target.value)}/>
                     </Form.Group>
                 </Row>
 
                 <Row className="mb-3">
                     <Form.Group as={Col} controlId="formGridAddress2">
                         <Form.Label>Phone number</Form.Label>
-                        <Form.Control placeholder="000 000 000" value={loggedInUser.phoneNumber}/>
+                        <Form.Control placeholder="000 000 000" value={phoneNumber}  onChange={(e) => setPhoneNumber(e.target.value)}/>
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridAddress1">
                         <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="1234 Main St" value={loggedInUser.farmerAdress}/>
+                        <Form.Control placeholder="1234 Main St" value={farmerAdress} onChange={(e) => setFarmerAdress(e.target.value)}/>
                     </Form.Group>
                 </Row>
 
                 <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridCity" value={loggedInUser.city}>
+                    <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>City</Form.Label>
-                    <Form.Control placeholder="Lisbon"/>
+                    <Form.Control placeholder="Lisbon" value={city} onChange={(e) => setCity(e.target.value)}/>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridZip" value={loggedInUser.zipCode}>
+                    <Form.Group as={Col} controlId="formGridZip">
                     <Form.Label>Zip</Form.Label>
-                    <Form.Control placeholder="1000-000"/>
+                    <Form.Control placeholder="1000-000" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
                     </Form.Group>
                 </Row>
 
